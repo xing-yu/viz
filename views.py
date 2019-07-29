38756fc4bc0a32a656056faba472bcee33d786e2
xing-yu <staryu.udi@gmail.com>
2019-07-28 20:55:22 -0400

    2nd commit

--- views.py
+++ views.py
@@ -5,9 +5,29 @@ import json
 def graph(request):
 	# data = json.load(open("/Users/Xing/Documents/vpo/viz/data/graph_data.json", 'r'))
 
-	data = json.load(open("/Users/Xing/Documents/vpo/viz/data/graph_data_euclidean.txt", 'r'))
+	graph_data = json.load(open("/Users/Xing/Documents/vpo/viz/data/graph_data_euclidean.txt", 'r'))
 
-	context = {"graph_data": json.dumps(data)}
+	texts_data = json.load(open("/Users/Xing/Documents/vpo/viz/data/texts_data.txt", 'r'))
+
+	filter_texts_data = []
+
+	for line in texts_data:
+
+		if len(line) == 0: continue
+
+		no_tag = True
+
+		for each in line:
+
+			if each['tag'] != 'O':
+
+				no_tag = False
+
+		if no_tag == False:
+
+			filter_texts_data.append(line) 
+
+	context = {"graph_data": json.dumps(graph_data), "texts_data": json.dumps(filter_texts_data)}
 
 	return render(request, "viz/graph.html", context)
 
